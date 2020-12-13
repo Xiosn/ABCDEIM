cd56fd4523f49a942517ff8f9e8d612c97e2f64d
Xiosn <Xiosn@github.com>
2020-12-13 19:24:18 +0800

    优化图片展示

--- packages/components/message/image.vue
+++ packages/components/message/image.vue
@@ -1,5 +1,5 @@
 <script>
-import close from '../../icon/close.png'
+import close from '../../../assets/close.png'
 export default {
   name: "lemonMessageImage",
   inheritAttrs: false,
@@ -17,25 +17,21 @@ export default {
         class="lemon-message-image"
         props={{ ...this.$attrs }}
         scopedSlots={{
-          content: props => <img src={props.content} on-click={
-            e=>{
-              this.content=props.content;
-              this.isShow=true
-            }
+          content: props => <img style="max-width:100%;min-width:100px;display:block;" src={props.content} on-click={()=>{this.content=props.content;this.isShow=true}
           }/>
         }}
       />
 
-      {this.isShow?<div class="box">
-          <img style="width:25px;height:25px;margin:10px 0 0 740px;cursor:pointer;" src={this.img} on-click={()=>{this.isShow=false}}/>
-          <img style="width:100%;height:91%; padding-top:10px;" src={this.content}/>
+      {this.isShow?<div style="position:absolute;left: -250px;right: -2px;top :-15px; padding: 0px 10px;padding-bottom:10px;z-index :9999;border-radius: 15px;border: #999;background :#fff;">
+          <img style="width:25px;height:25px;margin:10px 0 0 740px;cursor:pointer;z-index:9999;" src={this.img} on-click={()=>{this.isShow=false}}/>
+          <img style="max-width:100%;max-height:91%; width: auto; height: auto; padding-top:10px;z-index:99999" src={this.content}/>
       </div>:''}
       </div>
     );
   }
 };
 </script>
-<style lang="stylus">
+<style lang="stylus" scoped>
 @import '~styles/utils/index'
 +b(lemon-message-image)
   +b(lemon-message)
@@ -43,24 +39,4 @@ export default {
       padding 0
       cursor pointer
       overflow hidden
-      img
-        max-width 100%
-        min-width 100px
-        display block
-.box {
-  position absolute
-  left -250px
-  right -2px
-  top -15px
-  bottom -15px
-  padding 0px 10px
-  z-index 9999
-  border-radius 15px
-  border #999;
-  background #fff
-}
-img {
-  z-index:9999
-  right 0
-}
 </style>
